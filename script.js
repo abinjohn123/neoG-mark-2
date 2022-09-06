91cf4ecc137fb77ed861c3a10cb9be4d6216a3fe
Abin John <abinjohnanto@gmail.com>
2022-09-06 18:40:47 +0530

    chore: add questions list

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 const R = require('readline-sync');
+
+const questions = [
+  {
+    question: 'Which of these brands is Indian? (Toyota, Datsun, Tata): ',
+    answer: 'tata',
+  },
+  {
+    question: "What is the name of Elon Musk's car company?: ",
+    answer: 'tesla',
+  },
+  {
+    question: "Which manufacturer's car is the Innova?: ",
+    answer: 'toyota',
+  },
+  {
+    question: "Which manufacturer's car is the Mustang?: ",
+    answer: 'ford',
+  },
+  {
+    question: 'Hyundai and Kia are based out of _______: ',
+    answer: 'korea',
+  },
+];
